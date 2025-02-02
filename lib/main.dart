26f90254f0d895b37994b33d49f6c8f39fe4390d
Coding Rooms <git@codingrooms.io>
2025-02-02 16:08:51 +0000

    Initial commit of the Flutter calculator app

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:expressions/expressions.dart';
+
+void main() {
+  runApp(const CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator - Your Name',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _expression = "";
+  String _result = "0";
+
+  void _onPressed(String value) {
+    setState(() {
+      if (value == "C") {
+        _expression = "";
+        _result = "0";
+      } else if (value == "=") {
+        try {
+          final expression = Expression.parse(_expression);
+          final evaluator = const ExpressionEvaluator();
+          final result = evaluator.eval(expression, {});
+          _result = result.toString();
+          _expression += " = $_result";
+        } catch (e) {
+          _result = "Error";
+        }
+      } else {
+        _expression += value;
+      }
+    });
+  }
+
+  Widget _buildButton(String value) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () => _onPressed(value),
+        child: Text(
+          value,
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Calculator - Your Name')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              alignment: Alignment.bottomRight,
+              child: Text(
+                _expression,
+                style: const TextStyle(fontSize: 32),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              alignment: Alignment.bottomRight,
+              child: Text(
+                _result,
+                style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          ...[
+            ['7', '8', '9', '/'],
+            ['4', '5', '6', '*'],
+            ['1', '2', '3', '-'],
+            ['C', '0', '=', '+'],
+          ].map(
+            (row) => Row(
+              children: row.map((value) => _buildButton(value)).toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
